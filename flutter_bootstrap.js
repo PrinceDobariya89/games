58c90aa6e9ec1a45da8f553e169039847e05dd60
Prince Dobariya <pbdobariya2002@gmail.com>
2025-06-07 08:30:40 +0530

    initial commit

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"18818009497c581ede5d8a3b8b833b81d00ceb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3704701890"
+    serviceWorkerVersion: "2465439124"
   }
 });
