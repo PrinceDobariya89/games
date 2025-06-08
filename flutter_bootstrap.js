83292561fbfcdc48449601b58000389e1a86c6d7
Prince Dobariya <pbdobariya2002@gmail.com>
2025-06-08 23:43:56 +0530

    line

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"18818009497c581ede5d8a3b8b833b81d00ceb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2465439124"
+    serviceWorkerVersion: "1552469715"
   }
 });
